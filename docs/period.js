13c318f259a0c57f770c4ba0b77f7daa9c201658
AYAYze <ehdud2004@gmail.com>
2020-07-08 20:35:39 +0900

    Change Desingn

--- docs/period.js
+++ docs/period.js
@@ -11,7 +11,7 @@ async function makeStr() {
         .getElementById('str')
         .value;
     if(!eng.test(str)){
-        console.log("영어만 부탁해~")
+        //console.log("영어만 부탁해~")
         alert('영어만 부탁해~');
         return;
     }
@@ -28,7 +28,7 @@ async function makeStr() {
         json.forEach((value, index) => {
             if ((str.substring(0, 2)).toLowerCase() == value.char.toLowerCase()) {
                 two = true;
-                console.log(value.char + ' ' + index);
+                //console.log(value.char + ' ' + index);
                 dividedStr[count] = {
                     num : value.num,
                     char : value.char,
@@ -42,7 +42,7 @@ async function makeStr() {
             json.forEach((value, index) => {
                 if ((str.substring(0, 1)).toLowerCase() == value.char.toLowerCase()) {
                     one = true;
-                    console.log(value.char + ' ' + index);
+                    //console.log(value.char + ' ' + index);
                     dividedStr[count] = {
                         num : value.num,
                         char : value.char,
@@ -62,13 +62,14 @@ async function makeStr() {
         two = false;
     }
         
-    console.log(dividedStr)
+    //console.log(dividedStr)
     let form = '';
     dividedStr.forEach((value, index) => {
         let a = value.num;
         let b = value.char;
         let c = value.name;
-        let d = value.color;
+        let d = value.color?.substr(1,6);
+        let e = value.color?.substr(1,6);
         let nonBorder = '';
         console.log(b);
         if(b == undefined){
@@ -77,9 +78,17 @@ async function makeStr() {
             c = ' '
             nonBorder = 'border:none;width:30px';
         }
-
-        form += '<div class="period" style="background-color:' + d + ';'+nonBorder+'"><div class="num">' + a + '</div><div class="chr">' + b + '</div><div class="name">' + c + '</div></div>'
+        let rgbaForBox = {
+            r: parseInt(d?.substr(0,2),16),
+            g: parseInt(d?.substr(2,2),16),
+            b: parseInt(d?.substr(4,2),16)
+        } 
+        console.log(d);
+        //console.log(rgbaForBox);
+        //form += '<div class="period" style="background-color:' + d + ';'+nonBorder+';rgba("><div class="num">' + a + '</div><div class="chr">' + b + '</div><div class="name">' + c + '</div></div>'
+        form += `<div class="period" style="background-color:#${d};${nonBorder};box-shadow:10px 10px 15px rgba(${rgbaForBox.r-20},${rgbaForBox.g-20},${rgbaForBox.b-20},0.9),-10px -10px 15px rgba(${rgbaForBox.r+20}, ${rgbaForBox.g+20}, ${rgbaForBox.b+20},0.5),inset 10px 10px 30px transparent,inset -10px -10px 30px transparent;" onmouseover="this.style.boxShadow='10px 10px 30px transparent,-10px -10px 30px transparent,inset 10px 10px 30px rgba(${rgbaForBox.r - 20}, ${rgbaForBox.g - 20}, ${rgbaForBox.b - 20}, 0.9),inset -10px -10px 30px rgb(${rgbaForBox.r + 20}, ${rgbaForBox.g + 20}, ${rgbaForBox.b + 20})'" onmouseout="this.style.boxShadow='10px 10px 15px rgba(${rgbaForBox.r-20},${rgbaForBox.g-20},${rgbaForBox.b-20},0.9),-10px -10px 15px rgba(${rgbaForBox.r+20}, ${rgbaForBox.g+20}, ${rgbaForBox.b+20},0.5),inset 10px 10px 30px transparent,inset -10px -10px 30px transparent'"><div class="num">${a}</div><div class="chr">${b}</div><div class="name">${c}</div></div>`
         document.getElementById('builtStr').innerHTML = form;
+
     })
     
 
